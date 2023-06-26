ddf3ab6e7bffe7bb3fb437c93a2d8f29a4cbb596
frolleks <frolleks@hotmail.com>
2023-06-26 16:15:41 +0700

    feat(auth): add a callback that returns the user id

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,34 @@
-import type { NextAuthOptions } from "next-auth";
+import type { DefaultSession, NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { env } from "@/env.mjs";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
 
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
 export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
   adapter: PrismaAdapter(db),
   providers: [
     GithubProvider({
