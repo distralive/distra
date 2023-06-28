709b92d14137d2d23a07331c57f3bb42b192704c
frolleks <frolleks@hotmail.com>
2023-06-28 12:09:57 +0700

    feat: use channel and video card in user page

--- app/user/[slug]/page.tsx
+++ app/user/[slug]/page.tsx
@@ -1,9 +1,18 @@
+import { ChannelCard } from "@/components/channel-card";
+import { VideoCard } from "@/components/video-card";
 import { db } from "@/lib/db";
 
 async function getUser(id: string) {
   const user = await db.user.findUnique({
     where: {
       id,
+    },
+    select: {
+      id: true,
+      name: true,
+      username: true,
+      image: true,
+      videos: true,
     },
   });
 
@@ -15,7 +24,15 @@ export default async function User({ params }: { params: { slug: string } }) {
 
   return data ? (
     <>
-      <div>{data.name}</div>
+      <ChannelCard id={params.slug} data={data} />
+      <div className="flex flex-col p-3 px-12">
+        <p className="text-xl mb-1.5 font-semibold">Videos</p>
+        <div className="flex space-x-1.5 overflow-x-auto whitespace-nowrap">
+          {data.videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </div>
+      </div>
     </>
   ) : (
     <div>404</div>
