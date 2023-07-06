e1052f982ab580205055844a4052a4311c0e3c4d
frolleks <frolleks@hotmail.com>
2023-07-06 19:08:32 +0700

    feat: add a route for deleting videos

--- app/api/video/[slug]/route.ts
+++ app/api/video/[slug]/route.ts
@@ -1,4 +1,6 @@
+import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
 
 export async function GET(
   req: Request,
@@ -33,5 +35,72 @@ export async function GET(
       status: 404,
       headers: { "Content-Type": "application/json" },
     });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { slug: string } }
+) {
+  const id = params.slug;
+  const session = await getServerSession(authOptions);
+
+  try {
+    if (session) {
+      const video = await db.video.findUnique({
+        where: {
+          id,
+        },
+        select: {
+          id: true,
+          authorId: true,
+        },
+      });
+      if (video?.authorId === session?.user.id) {
+        await db.video.delete({
+          where: {
+            id,
+          },
+        });
+
+        return new Response(JSON.stringify({ success: true }), {
+          status: 200,
+          headers: { "Content-Type": "application/json" },
+        });
+      } else {
+        return new Response(
+          JSON.stringify({
+            success: false,
+            error: "You must be the author of the video to delete it.",
+          }),
+          {
+            status: 401,
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+      }
+    } else {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          error: "You must be logged in to delete a video.",
+        }),
+        {
+          status: 401,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error,
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
 }
