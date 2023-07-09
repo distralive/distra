7bf411d2e15c2d8938a6794d0872e4ac52e3af37
frolleks <frolleks@hotmail.com>
2023-07-09 21:00:38 +0700

    feat: add hamburger-menu to user page

--- app/user/[slug]/page.tsx
+++ app/user/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { ChannelCard } from "@/components/channel-card";
+import { HamburgerMenu } from "@/components/hamburger-menu";
 import { VideoCard } from "@/components/video-card";
 import { db } from "@/lib/db";
 
@@ -24,17 +25,22 @@ export default async function User({ params }: { params: { slug: string } }) {
   const data = await getUser(params.slug);
 
   return data ? (
-    <>
-      <ChannelCard id={params.slug} data={data} />
-      <div className="flex flex-col p-3 px-12">
-        <p className="text-xl mb-1.5 font-semibold">Videos</p>
-        <div className="flex space-x-1.5 overflow-x-auto whitespace-nowrap">
-          {data.videos.map((video) => (
-            <VideoCard key={video.id} video={video} />
-          ))}
+    <div className="flex">
+      <div className="flex-shrink-0">
+        <HamburgerMenu />
+      </div>
+      <div className="flex flex-col w-full">
+        <ChannelCard id={params.slug} data={data} />
+        <div className="flex flex-col p-3 px-12">
+          <p className="text-xl mb-1.5 font-semibold">Videos</p>
+          <div className="flex space-x-1.5">
+            {data.videos.map((video) => (
+              <VideoCard key={video.id} video={video} />
+            ))}
+          </div>
         </div>
       </div>
-    </>
+    </div>
   ) : (
     <div>404</div>
   );
