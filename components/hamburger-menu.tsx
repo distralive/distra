321ba4ab568ef641b0d3be758b96b23634af60f2
frolleks <frolleks@hotmail.com>
2023-07-09 20:58:13 +0700

    feat: add following user pfp if the user has one

--- components/hamburger-menu.tsx
+++ components/hamburger-menu.tsx
@@ -3,6 +3,7 @@ import { Icon } from "@/components/iconify-icon";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import Image from "next/image";
 
 async function getFollowing() {
   const session = await getServerSession(authOptions);
@@ -32,7 +33,7 @@ export async function HamburgerMenu() {
       className="w-64 border-r overflow-auto"
       style={{ height: "calc(100vh - 64px)" }}
     >
-      <div className="flex flex-col">
+      <div className="flex flex-col w-full">
         <div className="flex flex-col border-b space-y-1.5 p-2">
           <Link href="/">
             <div className="flex items-center p-1.5 space-x-2 w-full hover:bg-accent transition-colors rounded-md">
@@ -52,7 +53,16 @@ export async function HamburgerMenu() {
                   key={following.followed.id}
                 >
                   <div className="flex items-center py-1.5 space-x-2 w-full hover:bg-accent transition-colors rounded-md">
-                    <Icon icon="bx:user" fontSize={28} />
+                    {following.followed.image ? (
+                      <Image
+                        alt={`${following.followed.name}'s profile picture`}
+                        src={following.followed.image}
+                        width={28}
+                        height={28}
+                      />
+                    ) : (
+                      <Icon icon="bx:user" fontSize={28} />
+                    )}
                     <p className="text-sm">{following.followed.name}</p>
                   </div>
                 </Link>
