01bc305491ab7ab39e0d9f8c61a2c98339468616
frolleks <frolleks@hotmail.com>
2023-07-09 21:00:20 +0700

    refactor: make the div have full width

--- components/channel-card.tsx
+++ components/channel-card.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FollowButton } from "@/components/follow-button";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-export function ChannelCard({ id, data }: { id: string; data: any }) {
+export async function ChannelCard({ id, data }: { id: string; data: any }) {
+  const session = await getServerSession(authOptions);
+
   return (
-    <div className="border-b px-12 py-4 flex justify-between">
+    <div className="border-b px-12 py-4 flex justify-between w-full">
       <div className="flex items-center gap-6 justify-start">
         <Image
           src={data.image ?? ""}
@@ -29,7 +33,7 @@ export function ChannelCard({ id, data }: { id: string; data: any }) {
         </div>
       </div>
       <div className="flex items-center justify-end">
-        <FollowButton userId={id} />
+        <FollowButton userId={id} session={session} />
       </div>
     </div>
   );
