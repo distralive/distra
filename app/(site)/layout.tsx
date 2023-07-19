73a0ed1f36e7504658549c3f6bfe7f62fef834ae
frolleks <frolleks@hotmail.com>
2023-07-19 14:13:23 +0700

    refactor: add session prop for the header

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,14 +1,18 @@
 import { Header } from "@/components/header";
 import { Sidebar } from "@/components/sidebar";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
-export default function SiteLayout({
+export default async function SiteLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <>
-      <Header />
+      <Header session={session} />
       <Sidebar>{children}</Sidebar>
     </>
   );
