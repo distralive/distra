c537a5d2e02bb1791fdca834261d80cc89b44053
frolleks <frolleks@hotmail.com>
2023-07-19 14:08:52 +0700

    feat(auth): add id callback

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,11 +2,41 @@ import { db } from "@/lib/db";
 import { env } from "@/env.mjs";
 
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import type { AuthOptions } from "next-auth";
+import type { AuthOptions, DefaultSession } from "next-auth";
 
 import GoogleProvider from "next-auth/providers/google";
 
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: DefaultSession["user"] & {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    };
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
 export const authOptions: AuthOptions = {
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
   // @ts-expect-error Nullable Adapter
   adapter: PrismaAdapter(db),
   providers: [
