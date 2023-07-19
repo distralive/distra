8a6e227f16a82fab7838f8fa9bf34d648cd50a92
frolleks <frolleks@hotmail.com>
2023-07-19 12:06:38 +0700

    feat(auth): add google oauth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,18 @@
 import { db } from "@/lib/db";
+import { env } from "@/env.mjs";
 
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import type { AuthOptions } from "next-auth";
 
-const authOptions: AuthOptions = {
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
   // @ts-expect-error Nullable Adapter
   adapter: PrismaAdapter(db),
-  providers: [],
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 };
