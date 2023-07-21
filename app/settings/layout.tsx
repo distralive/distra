47124c84d7cf04caab688cae909c076e6f99c736
frolleks <frolleks@hotmail.com>
2023-07-21 19:44:57 +0700

    refactor: make the settings page not replace itself with images

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -52,23 +52,7 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
           </Link>
         </div>
       </div>
-      <div className="md:hidden">
-        <Image
-          src="/examples/forms-light.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/forms-dark.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
+      <div className="space-y-6 p-10 pb-16 md:block">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
           <p className="text-muted-foreground">
